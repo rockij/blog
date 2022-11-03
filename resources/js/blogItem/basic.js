fb5bee4a90749ad03aecae2a82c9813af240df77
rockij <rockij83@nate.com>
2022-11-04 07:17:40 +0900

    221104

---
+++ resources/js/blogItem/basic.js
@@ -0,0 +1,56 @@
+let basic = [
+  {
+    productName: "item정렬 방식 flex vs grid",
+    category: "basic",
+    url: "basic/flex_grid_item_layout_compare.html",
+  },
+  {
+    productName: "CSS로 목록 스타일을 개선하는 2가지 방법",
+    category: "basic",
+    url: "basic/2ways_improve_list_styling.html",
+  },
+  {
+    productName: "Testing the New CSS Reset",
+    category: "basic",
+    url: "https://codepen.io/robinrendle/pen/VwzPJrL?editors=1100",
+    linkType: 'outlink',
+  },
+  {
+    productName: "처음으로 CSS 그리드 배우기",
+    category: "basic",
+    url: "https://dev.to/thouravi/learning-css-grid-for-the-first-time-5amb",
+    linkType: 'outlink',
+  },
+  {
+    productName: "Div 요소의 내용 인쇄",
+    category: "basic",
+    url: "basic/print_contents_div_element.html",
+  },
+  {
+    productName: "grid를 이용한 덜 절대적인 위치 잡기",
+    category: "basic",
+    url: "basic/grid/absolute_positioning_modern.html",
+  },
+  {
+    productName: "@supports selector()",
+    category: `${'basic','css-trick'}`,
+    url: "https://css-tricks.com/supports-selector/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "css focus-visible(:focus 대신 사용)",
+    category: `${'basic', 'mouse-effect'}`,
+    url: "basic/css_focus-visible(instead-focus).html",
+  },
+  {
+    productName: "css all 속성에 대해서",
+    category: "basic",
+    url: "basic/css_all.html",
+  },
+  // {
+  //   productName: "",
+  //   category: "basic",
+  //   url: "",
+  //   linkType: 'outlink',
+  // },
+]
---
+++ resources/js/blogItem/workspace.js
@@ -0,0 +1,30 @@
+let workspace = [
+  {
+    productName: "guide - 웹퍼블리싱 가이드 sample",
+    category: "workspace",
+    url: "https://rockij.github.io/portfolio/project/min/010pay/resources/html/ui/template.html",
+  },
+  {
+    productName: "CSS 변수 모음 Open-Props",
+    category: "workspace",
+    url: "https://open-props.style/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "guide - 작업물 보기 리스트(worksheet)",
+    category: "workspace",
+    url: "basic/worksheet.html",
+  },
+  {
+    productName: "WebPageTest를 사용한 성능 테스트",
+    category: "workspace",
+    url: "https://webpagetest.org/",
+    linkType: 'outlink',
+  },
+  // {
+  //   productName: "",
+  //   category: "workspace",
+  //   url: "",
+  //   linkType: 'outlink',
+  // },
+]
