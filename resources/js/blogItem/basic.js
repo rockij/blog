daf671a311331ee418287a51abdf92d105e9315b
rockij <rockij83@nate.com>
2022-11-14 00:43:39 +0900

    221114

--- resources/js/blogItem/basic.js
+++ resources/js/blogItem/basic.js
@@ -1,7 +1,7 @@
 let basic = [
   {
     productName: "item정렬 방식 flex vs grid",
-    category: "basic",
+    category: `${'flex-grid'}`,
     url: "basic/flex_grid_item_layout_compare.html",
   },
   {
@@ -16,19 +16,19 @@ let basic = [
     linkType: 'outlink',
   },
   {
-    productName: "처음으로 CSS 그리드 배우기",
-    category: "basic",
+    productName: "처음으로 css grid 배우기",
+    category: `${'flex-grid'}`,
     url: "https://dev.to/thouravi/learning-css-grid-for-the-first-time-5amb",
     linkType: 'outlink',
   },
   {
-    productName: "Div 요소의 내용 인쇄",
+    productName: "div 요소의 내용 인쇄",
     category: "basic",
     url: "basic/print_contents_div_element.html",
   },
   {
     productName: "grid를 이용한 덜 절대적인 위치 잡기",
-    category: "basic",
+    category: `${'flex-grid'}`,
     url: "basic/grid/absolute_positioning_modern.html",
   },
   {
@@ -39,7 +39,7 @@ let basic = [
   },
   {
     productName: "css focus-visible(:focus 대신 사용)",
-    category: `${'basic', 'mouse-effect'}`,
+    category: `${'mouse-effect'}`,
     url: "basic/css_focus-visible(instead-focus).html",
   },
   {
@@ -47,9 +47,261 @@ let basic = [
     category: "basic",
     url: "basic/css_all.html",
   },
+  {
+    productName: "반응형 전체 화면 배경",
+    category: `${'background-border'}`,
+    url: "basic/responsive_full-screen_background.html",
+  },
+  {
+    productName: "논리적 속성을 사용하여 더 나은 CSS 작성",
+    category: "basic",
+    url: "basic/write_better_css_logical_properties.html",
+  },
+  {
+    productName: "투명한 PNG 이미지 그림자 적용",
+    category: `${'image', 'background-border'}`,
+    url: "basic/shadows_transparent_PNG.html",
+  },
+  {
+    productName: "프로젝트에서 사용할 수 있는 flexbox 디자인 패턴",
+    category: `${'flex-grid'}`,
+    url: "basic/flex/flexbox_design_patterns_projects.html",
+  },
+  {
+    productName: "css auto-margin 정렬",
+    category: `${'flex-grid', 'layout'}`,
+    url: "basic/auto_margin.html",
+  },
+  {
+    productName: "그리드로 쉽게 콘텐츠 겹침",
+    category: `${'flex-grid'}`,
+    url: "basic/grid/easily_overlap_content.html",
+  },
+  {
+    productName: "배경 비디오를 만드는 방법",
+    category: `${'background-border'}`,
+    url: "basic/how_to_create_background_video.html",
+  },
+  {
+    productName: "background-blend-mode에 대해서",
+    category: `${'background-border'}`,
+    url: "basic/css_background_blend_mode.html",
+  },
+  {
+    productName: "css calc() 에 대해서",
+    category: "basic",
+    url: "basic/css_calc().html",
+  },
+  {
+    productName: "css object-fit을 사용하여 이미지를 제어하는 방법",
+    category: `${'image'}`,
+    url: "basic/css_object-fit.html",
+  },
+  {
+    productName: "미디어 쿼리가 없는 반응형 CSS 그리드!",
+    category: `${'flex-grid', 'layout'}`,
+    url: "basic/grid/responsive_no_media_queries.html",
+  },
+  {
+    productName: "배경반복 값 space에 대해서",
+    category: `${'background-border'}`,
+    url: "basic/background_repeat_space.html",
+    linkType: 'outlink',
+  },
+  {
+    productName: "다른 색상의 CSS 이중 테두리",
+    category: `${'background-border'}`,
+    url: "basic/double_border_width_diffrent_colors.html",
+  },
+  {
+    productName: "영리한 고정 바닥글 기법",
+    category: `${'layout'}`,
+    url: "basic/sticky_footer_technique.html",
+  },
+  {
+    productName: "그림자 그라디언트",
+    category: `${'background-border'}`,
+    url: "basic/grdient_drop_shadow.html",
+  },
+  {
+    productName: "줄무늬 배경",
+    category: `${'background-border'}`,
+    url: "basic/stiped_background_pattern.html",
+  },
+  {
+    productName: "도트 배경",
+    category: `${'background-border'}`,
+    url: "basic/dot_background_pattern.html",
+  },
+  {
+    productName: "css touch-action 한눈에보기",
+    category: `${'mouse-effect'}`,
+    url: "basic/touch-action.html",
+  },
+  {
+    productName: "css 가상요소 선택자 - marker",
+    category: `${'basic'}`,
+    url: "basic/css가상요소_marker.html",
+  },
+  {
+    productName: "CSS scroll-behavior",
+    category: `${'css-trick', 'scroll-parallax'}`,
+    url: "https://css-tricks.com/almanac/properties/s/scroll-behavior/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "CSS 재설정으로 :where() 의 특수성 사용",
+    category: `${'css-trick', 'basic'}`,
+    url: "https://css-tricks.com/using-the-specificity-of-where-as-a-css-reset/",
+    linkType: 'outlink',
+  },
+  {
+    productName: ":fullscreen 선택자",
+    category: `${'css-trick', 'basic'}`,
+    url: "https://css-tricks.com/almanac/selectors/f/fullscreen/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "mix-blend-mode 사용 예시 (isolation)",
+    category: `${'background-border'}`,
+    url: "basic/css_mix-blend-mode_사용.html",
+  },
+  {
+    productName: "inset을 이용한 full 레이아웃",
+    category: `${'layout'}`,
+    url: "basic/css_inset_layout.html",
+  },
+  {
+    productName: "input에 관하여",
+    category: `${'form', 'basic'}`,
+    url: "basic/input.html",
+  },
+  {
+    productName: "유동적인 타이포그래피 css함수 - clamp()",
+    category: `${'text-effect'}`,
+    url: "basic/유동적인_타이포그래피_clamp().html",
+  },
+  {
+    productName: "반전 그림자 표현 reflected text",
+    category: `${'text-effect'}`,
+    url: "basic/reflected_text.html",
+  },
+  {
+    productName: "skeleton screen",
+    category: `${'icon-loading'}`,
+    url: "basic/skeleton_screen.html",
+  },
+  {
+    productName: "skeleton screen 여러개",
+    category: `${'icon-loading'}`,
+    url: "basic/skeleton_screen2.html",
+  },
+  {
+    productName: "flex margin-top 이용한 footer고정",
+    category: `${'layout'}`,
+    url: "basic/footer_flex(margin).html",
+  },
+  {
+    productName: "와인샵 컨셉_반응형 슬라이더(GREENSOCK)",
+    category: `${'slide'}`,
+    url: "slide/wine_shop_concept_responsive_slider.html",
+  },
+  {
+    productName: "응답성이 뛰어난 최소 포트폴리오 웹 사이트",
+    category: `${'slide'}`,
+    url: "slide/minimal_portfolio/index.html",
+  },
+  {
+    productName: "고급스러운 반응형 슬라이드 slick.js",
+    category: `${'slide'}`,
+    url: "slide/responsive_slider_slickjs/index.html",
+  },
+  {
+    productName: "gsap를 이용한 세로로 부드럽게 슬라이드",
+    category: `${'slide'}`,
+    url: "slide/vertical_content_slider.html",
+  },
+  {
+    productName: "수직 카드 슬라이딩 애니메이션(only css)",
+    category: `${'slide'}`,
+    url: "slide/vertical_card_sliding_animation.html",
+  },
+  {
+    productName: "계속 반복되는 한줄이미지 배너(only css)",
+    category: `${'slide'}`,
+    url: "slide/infinite_autoplay_carousel(only-css).html",
+  },
+  {
+    productName: "멀티플 슬라이드 순수 자바스크립트",
+    category: `${'slide'}`,
+    url: "slide/multiple_slideshow_loop.html",
+  },
+  {
+    productName: "이미지 반사 효과 3D 슬라이더",
+    category: `${'slide'}`,
+    url: "slide/3D_image_slider_reflection_Materialize.html",
+    linkType: 'outlink',
+  },
+  {
+    productName: "Glide.js를 사용한 놀라운 효과의 이미지 슬라이드",
+    category: `${'slide'}`,
+    url: "slide/glide/index.html",
+  },
+  {
+    productName: "Javascript를 사용 Mousemove에서 이미지 회전 제어",
+    category: `${'slide'}`,
+    url: "slide/control_image_rotation_mousemove.html",
+  },
+  {
+    productName: "circular rotating reflection",
+    category: `${'slide'}`,
+    url: "slide/css_circular_rotating_slider_reflection.html",
+  },
+  {
+    productName: "swiper 슬라이드 IE 호환성 문제 해결",
+    category: `${'slide'}`,
+    url: "slide/swiper-4.5.1/demos/010-default.html",
+    linkType: 'outlink',
+  },
+  {
+    productName: "종속성 없이 순수 JavaScript로 작성된 가볍고 강력하며 유연한 슬라이더(splide)",
+    category: `${'slide'}`,
+    url: "https://splidejs.com/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "가장 현대적인 모바일 터치 슬라이더 - swiper",
+    category: `${'slide'}`,
+    url: "https://swiperjs.com/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "js가 가벼운 자동정지/시작이 가능한 슬라이드(glide)",
+    category: `${'slide'}`,
+    url: "https://glidejs.com/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "js가 가벼운 슬라이드(glider)",
+    category: `${'slide'}`,
+    url: "https://nickpiscitelli.github.io/Glider.js/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "오직css만을 이용한 슬라이드(auto)",
+    category: `${'slide'}`,
+    url: "slide/css-only_carousel_slider.html",
+    linkType: 'outlink',
+  },
+  {
+    productName: "canvas + javascript를 이용한 부드럽게 그라디언트 움직임",
+    category: `${'background-border'}`,
+    url: "page-bg/background_gradient_embraced.html",
+    linkType: 'outlink',
+  },
   // {
   //   productName: "",
-  //   category: "basic",
+  //   category: `${'', ''}`,
   //   url: "",
   //   linkType: 'outlink',
   // },
--- resources/js/blogItem/workspace.js
+++ resources/js/blogItem/workspace.js
@@ -21,6 +21,42 @@ let workspace = [
     url: "https://webpagetest.org/",
     linkType: 'outlink',
   },
+  {
+    productName: "normalize.css",
+    category: "workspace",
+    url: "http://necolas.github.io/normalize.css/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "vscode emmet문법",
+    category: "workspace",
+    url: "https://docs.emmet.io/cheat-sheet/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "자동 접두사",
+    category: "workspace",
+    url: "https://autoprefixer.github.io/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "부모내에 어떤 자식요소 배치가 가능한지",
+    category: "workspace",
+    url: "https://caninclude.glitch.me/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "clip-path maker",
+    category: "workspace",
+    url: "https://bennettfeely.com/clippy/",
+    linkType: 'outlink',
+  },
+  {
+    productName: "다양한 브라우저 지원 - polyfill",
+    category: "workspace",
+    url: "https://polyfill.io/v3/",
+    linkType: 'outlink',
+  },
   // {
   //   productName: "",
   //   category: "workspace",
