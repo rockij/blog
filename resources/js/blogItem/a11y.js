b5c4089811ebe5f5329370844bab75c6e732a2a8
김익중 <rockij83@nate.com>
2022-12-14 20:54:10 +0900

    221214

---
+++ resources/js/blogItem/a11y.js
@@ -0,0 +1,12 @@
+let a11y = [
+  {
+    productName: 'ARIA 타이머 알림',
+    category: 'a11y',
+    url: 'html/a11y/timer_noti.html',
+  },
+  // {
+  //   productName: '',
+  //   category: 'a11y',
+  //   url: 'html/a11y/.html',
+  // },
+]
